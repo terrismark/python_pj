8264094b4b55ad9e490b982419c39677acb923c4
Mark Terris <terris_mark@mail.ru>
2020-10-22 18:12:27 +0400

    Making sure git is working fine)

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+"""
+This project is for testing python features and
+making test programs with different libraries.
+
+Enjoy programming!
+"""
+
+
 def print_hi(name):
+    """
+    This is a test function
+    :param name: string
+    :return: string
+    """
     print(name)
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Hello, world!')
 
